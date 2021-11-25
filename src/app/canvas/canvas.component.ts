e0bfebea384c5b51c91ed5a8bd39ab45e8cd5f37
Brian Stiner <Brian.Stiner@revature.net>
2021-11-24 21:55:27 -0500

    Added draw functions

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -25,10 +25,19 @@ export class CanvasComponent implements OnInit {
     brushColor = '#000000';                  //BrushColor is the color of the brush.
     brushOpacity = 1;                        //BrushOpacity is the opacity of the brush.
 
+    drawPoint = (e: MouseEvent) => {return void};
+    nStartX = 0;
+    nStartY = 0;
+    bIsDrawing = false;
+    nDeltaX = 0;
+    nDeltaY = 0;
+    radius = 10;
+
     ngOnInit() {
         this.ctx.fillStyle = '#000';
         this.ctx.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
     }
+
     ClearCanvas() {
         this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
     }
@@ -47,7 +56,6 @@ export class CanvasComponent implements OnInit {
         // delete the link link.delete;                               
     }
 
-
     RecordHistory() {
         this.hist.push(this.ctx.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height));
         this.memory = history.length - 1;
@@ -64,4 +72,119 @@ export class CanvasComponent implements OnInit {
             this.ClearCanvas();
             this.ctx.putImageData(this.hist[++this.memory], 0, 0); //memory++?
     }}
+
+
+
+
+
+
+    putPoint(e: MouseEvent){ // e is event: e.clientX and e.clientY are the coordinates of the mouse.
+        this.nStartX = e.clientX;
+        this.nStartY = e.clientY;
+        this.bIsDrawing = true;
+    }
+    stopPoint(){
+        this.bIsDrawing = false;
+        this.RecordHistory();
+    }
+    switchTool(choice:string, e: MouseEvent){
+        switch(choice){
+            case 'Pen':
+                this.drawPoint = this.drawPointPen(e);break;
+            case 'Eraser':
+                this.drawPoint = this.drawPointEraser(e);break;
+            case 'Circle':
+                this.drawPoint = this.drawPointCircle(e);break;
+            case 'Heart':
+                this.drawPoint = this.drawPointHeart(e);break;
+            case 'Line':
+                this.drawPoint = this.drawPointLine(e);break;
+            case 'Rectangle':
+                this.drawPoint = this.drawPointRectangle(e);break;
+            case 'Triangle':
+                this.drawPoint = this.drawPointTriangle(e);break;
+            case 'Square':
+                this.drawPoint = this.drawPointSquare(e);break;
+        }
+    }
+    
+    drawPointPen(e: MouseEvent){  // This draws on mouse
+        if(!this.bIsDrawing){
+            return;
+        }
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.nStartX, this.nStartY);
+        this.ctx.lineTo(e.clientX, e.clientY);
+        this.ctx.stroke();
+        this.nStartX = e.clientX;
+        this.nStartY = e.clientY;
+    }
+    drawPointEraser(e: MouseEvent){ // This erases on mouse
+        if(!this.bIsDrawing){return;}
+        this.ctx.globalCompositeOperation = 'destination-out';
+        this.ctx.beginPath();
+        this.ctx.arc(this.nStartX, this.nStartY, 10, 0, Math.PI*2);
+        this.ctx.fill();
+        this.ctx.globalCompositeOperation = 'source-over';
+        this.nStartX = e.clientX;
+        this.nStartY = e.clientY;
+    }
+    drawPointCircle(e: MouseEvent){  // This draws circles.
+        if(!this.bIsDrawing)
+            return;
+        this.nDeltaX = this.nStartX - e.clientX;
+        this.nDeltaY = this.nStartY - e.clientY;
+        this.radius = Math.sqrt(this.nDeltaX * this.nDeltaX + this.nDeltaY * this.nDeltaY)
+        this.ctx.beginPath();
+        this.ctx.arc(this.nStartX, this.nStartY, this.radius, 0, Math.PI*2);
+        this.ctx.fill();
+    }
+    drawPointHeart(e: MouseEvent){  // This draws hearts.
+        if(!this.bIsDrawing)
+            return;
+        this.nDeltaX = this.nStartX - e.clientX;
+        this.nDeltaY = this.nStartY - e.clientY;
+        this.radius = Math.sqrt(this.nDeltaX * this.nDeltaX + this.nDeltaY * this.nDeltaY)
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.nStartX, this.nStartY);
+        this.ctx.bezierCurveTo(this.nStartX + this.radius, this.nStartY, this.nStartX + this.radius, this.nStartY - this.radius, this.nStartX, this.nStartY - this.radius);
+        this.ctx.bezierCurveTo(this.nStartX - this.radius, this.nStartY - this.radius, this.nStartX - this.radius, this.nStartY, this.nStartX, this.nStartY);
+        this.ctx.fill();
+    }
+    drawPointLine(e: MouseEvent){  // This draws lines.
+        if(!this.bIsDrawing)
+            return;
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.nStartX, this.nStartY);
+        this.ctx.lineTo(e.clientX, e.clientY);
+        this.ctx.stroke();
+    }
+    drawPointRectangle(e: MouseEvent){  // This draws rectangles.
+        if(!this.bIsDrawing)
+            return;
+        this.ctx.beginPath();
+        this.ctx.rect(this.nStartX, this.nStartY, e.clientX - this.nStartX, e.clientY - this.nStartY);
+        this.ctx.fill();
+    }
+    drawPointTriangle(e: MouseEvent){  // This draws triangles.
+        if(!this.bIsDrawing)
+            return;
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.nStartX, this.nStartY);
+        this.ctx.lineTo(e.clientX, e.clientY);
+        this.ctx.lineTo(this.nStartX, e.clientY);
+        this.ctx.lineTo(this.nStartX, this.nStartY);
+        this.ctx.fill();
+    }
+    drawPointSquare(e: MouseEvent){  // This draws squares.
+        if(!this.bIsDrawing)
+            return;
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.nStartX, this.nStartY);
+        this.ctx.lineTo(e.clientX, e.clientY);
+        this.ctx.lineTo(this.nStartX, e.clientY);
+        this.ctx.lineTo(e.clientX, this.nStartY);
+        this.ctx.lineTo(this.nStartX, this.nStartY);
+        this.ctx.fill();
+    }
 }
