8648ccf19d1cbbdaa2c6281d4479cb258cdac3ef
Brian Stiner <Brian.Stiner@revature.net>
2021-11-24 22:46:32 -0500

    done for the night. Blank page with no canvas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
